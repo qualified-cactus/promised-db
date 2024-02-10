8a663716569762cba03bf480b5ae9343cc4da1d9
qualified-cactus <lemanhhieu.work@gmail.com>
2024-02-11 00:51:06 +0700

    3.0.0

--- src/dbActions.ts
+++ src/dbActions.ts
@@ -30,20 +30,9 @@ export class ObjectStore<T, PK extends IDBValidKey> {
     }
 
     /**
-     * Get an object with a primary key
-     * @param key the primary key of the object
-     * @returns undefined if no object is found
+     * @see {@link IDBObjectStore.get}
      */
-    get(key: PK): Promise<T | undefined> {
-        return dbPromise(this.#objectStore.get(key))
-    }
-
-    /**
-     * Find the first object in a primary key range 
-     * @param query the primary key range
-     * @returns undefined if no object is found
-     */
-    getFirstInRange(query: IndexKeyRange<PK>): Promise<T | undefined> {
+    get(query: PK | IndexKeyRange<PK>): Promise<T | undefined> {
         return dbPromise(this.#objectStore.get(query))
     }
 
@@ -51,7 +40,7 @@ export class ObjectStore<T, PK extends IDBValidKey> {
      * It is {@link get} but will throw if no object is found
      * @throws {NoResultError} when no object with {@link key} is found
      */
-    async requireGet(key: PK): Promise<T> {
+    async requireGet(key: PK | IndexKeyRange<PK>): Promise<T> {
         const result = await dbPromise(this.#objectStore.get(key))
         if (result !== undefined) {
             return result
@@ -61,29 +50,28 @@ export class ObjectStore<T, PK extends IDBValidKey> {
     /**
      * A {@link Promise} version of {@link IDBObjectStore.getAll}
      */
-    getAll(query?: IndexKeyRange<PK>, count?: number): Promise<T[]> {
+    getAll(query?: PK | IndexKeyRange<PK>, count?: number): Promise<T[]> {
         return dbPromise(this.#objectStore.getAll(query, count))
     }
 
     /**
-     * Get all primary keys of this object store.
-     * This method is a promise-version of {@link IDBObjectStore.getAllKeys}.
+     * @see {@link IDBObjectStore.getAllKeys}
      */
-    getAllPrimaryKeys(query?: IndexKeyRange<PK>, count?: number): Promise<PK[]> {
+    getAllPrimaryKeys(query?: PK| IndexKeyRange<PK>, count?: number): Promise<PK[]> {
         return dbPromise(this.#objectStore.getAllKeys(query, count)) as Promise<PK[]>
     }
 
     /**
-     * A {@link Promise} version of {@link IDBObjectStore.delete}
+     * @see {@link IDBObjectStore.getKey}
      */
-    delete(key: PK): Promise<undefined> {
-        return dbPromise(this.#objectStore.delete(key))
+    async getPrimaryKey(query: PK | IndexKeyRange<PK>): Promise<PK | undefined> {
+        return (await dbPromise(this.#objectStore.getKey(query))) as PK | undefined
     }
 
     /**
-     * A {@link Promise} version of {@link IDBObjectStore.delete}
+     * @see {@link IDBObjectStore.delete}
      */
-    deleteRange(query: IndexKeyRange<PK>): Promise<undefined> {
+    delete(query: PK | IndexKeyRange<PK>): Promise<undefined> {
         return dbPromise(this.#objectStore.delete(query))
     }
 
@@ -97,26 +85,34 @@ export class ObjectStore<T, PK extends IDBValidKey> {
     /**
      * A {@link Promise} version of {@link IDBObjectStore.count}
      */
-    count(query?: IndexKeyRange<PK>): Promise<number> {
+    count(query?: PK | IndexKeyRange<PK>): Promise<number> {
         return dbPromise(this.#objectStore.count(query))
     }
 
+    /**
+     * @see {ObjectStore.index}
+     */
     index<IT extends IDBValidKey>(indexDef: IndexDef<IT>): Index<T, IT, PK> {
         return new Index(this.#objectStore.index(indexDef.name))
     }
 
+    /**
+     * @see {ObjectStore.createIndex}
+     */
     createIndex<IT extends IDBValidKey>(indexDef: IndexDef<IT>): Index<T, IT, PK> {
         return new Index(this.#objectStore.createIndex(indexDef.name, indexDef.keyPath, indexDef.options))
     }
 
+    /**
+     * @see {ObjectStore.deleteIndex}
+     */
     deleteIndex<IT extends IDBValidKey>(indexDef: IndexDef<IT>) {
         this.#objectStore.deleteIndex(indexDef.name)
     }
 
     /**
      * Try to delete an index of the specified {@link name}. Return true if delete successfully.
-     * @param name 
-     * @returns 
+     * @see {ObjectStore.deleteIndex}
      */
     tryDeleteIndex(name: string): boolean {
         try {
@@ -144,42 +140,47 @@ export class Index<O, IK extends IDBValidKey, PK extends IDBValidKey> {
         this.iterator = new CursorIterator(index)
     }
 
-    get(key: IK): Promise<O | undefined> {
+    /**
+     * @see {IDBIndex.get}
+     */
+    get(key: IK | IndexKeyRange<IK>): Promise<O | undefined> {
         return dbPromise(this.#index.get(key))
-    }
-
-    getFirstInRange(query: IndexKeyRange<IK>): Promise<O | undefined> {
-        return dbPromise(this.#index.get(query))
     }
 
     /**
     * @throws {NoResultError} when no object with {@link key} is found
+    * @see {IDBIndex.get}
     */
-    async requireGet(key: PK): Promise<O> {
+    async requireGet(key: IK | IndexKeyRange<IK>): Promise<O> {
         const result = await dbPromise(this.#index.get(key))
         if (result !== undefined) {
             return result
         } else throw new NoResultError()
     }
 
-    getAll(query?: IndexKeyRange<IK>, count?: number): Promise<O[]> {
+    getAll(query?: IK | IndexKeyRange<IK>, count?: number): Promise<O[]> {
         return dbPromise(this.#index.getAll(query, count))
     }
 
-    getAllPrimaryKeys(query?: IndexKeyRange<PK>, count?: number): Promise<PK[]> {
+    /**
+     * @see {IDBIndex.getAll}
+     */
+    getAllPrimaryKeys(query?: IK | IndexKeyRange<IK>, count?: number): Promise<PK[]> {
         return dbPromise(this.#index.getAll(query, count))
     }
 
-    count(query?: IndexKeyRange<IK>): Promise<number> {
+    /**
+     * @see {IDBIndex.count}
+     */
+    count(query?: IK | IndexKeyRange<IK>): Promise<number> {
         return dbPromise(this.#index.count(query))
     }
 
-    getPrimaryKey(key: IK): Promise<PK | undefined> {
+    /**
+     * @see {IDBIndex.getKey}
+     */
+    getPrimaryKey(key: IK | IndexKeyRange<IK>): Promise<PK | undefined> {
         return dbPromise(this.#index.getKey(key)) as Promise<PK | undefined>
-    }
-
-    getFirstPrimaryKey(query: IndexKeyRange<IK>): Promise<PK | undefined> {
-        return dbPromise(this.#index.getKey(query)) as Promise<PK | undefined>
     }
 
 }
